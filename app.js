430ff8b218248940cbcec5b4cd1f7bb758a64bf5
Squinky <hey@squinky.me>
2018-01-27 12:45:57 -0500

    Added Jess's art assets

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function rotateKnob(v)
 {
 	$("#knob").rotate(v);
+	if (v == 360)
+	{
+		$("#dial").val(0);
+	}
+	else if (v == 0)
+	{
+		$("#dial").val(360);
+	}
 }
 
 $(document).ready(function()
