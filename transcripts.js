4738fb8d0ee29a2b73210f72551244f5883b6b4c
Jess Marcotte <meaikoh@gmail.com>
2018-01-27 16:09:19 -0500

    manifesto transcript complete
    -- added timestamps and appropriate transcript text for "manifesto.ogg"

--- transcripts.js
+++ transcripts.js
@@ -1,3 +1,4 @@
+
 var story =
 [
 	// #1: A T.R.A.N.S MANIFESTO
@@ -9,8 +10,18 @@ var story =
 			{ time: 0, text: "This is T.R.A.N.S Radio. We are the Trans Radio Artist Network in Space. We know you hear us. If you are one of us, this transmission is for you." },
 			{ time: 8.966, text: "Maybe you have known in the core of your being since you were three years old. Or maybe not." },
 			{ time: 13.509, text: "Maybe you discovered the language that describes you best when you were in your twenties, and it was like coming home, or finally being able to stretch out an ache." },
-			{ time: 21.263, text: "Maybe you have known for years, and maybe you came out to yourself yesterday." }
-			// add the rest here...
+			{ time: 21.263, text: "Maybe you have known for years, and maybe you came out to yourself yesterday." },
+			{ time: 25.437, text: "Maybe you hated to look at yourself in the mirror, once you learned enough to know what society told you you were, and what you weren't." },
+			{ time: 32.023, text: "Maybe you like your body just fine, and it's other people misgendering you that cuts to your core." },
+			{ time: 37.132, text: "Maybe your narrative isn't neatly wrapped up with any bow, and doesn't fit any mold." },
+			{ time: 41.691, text: "Maybe sometimes, you have to pretend that your gender identity is simpler than it is to get away from the questions, or for the comfort of those who don't understand you." },
+			{ time: 49.908, text: "Maybe you have tried to convince yourself that your physical reality is irrelevant, that your gender is meaningless, that you are just a consciousness." },
+			{ time: 57.642, text: "This probably isn't even the first time that you tune your long-range finders to this area, to this broadcast." },
+			{ time: 63.041, text: "Maybe you've been waiting, and trying to decide if you are going to make the leap, try out our little adventure, and see where you end up." },
+			{ time: 70.099, text: "No matter how you come to us, you are enough, you are enough, you are enough. You are trans enough, you are valid enough, you are simply...enough." },
+			{ time: 78.865, text: "We know that there's no such thing as a Utopia, but hey, whatever we make together can't be worse than here, right?"},
+			{ time: 85.407, text: "...Find us..." <br><br><br>"Eight. Nine. Five. Repeating transmission. Eight. Nine. Five."<br><br><br> "...Find us..."}
+						// add the rest here...
 		]
 	}
 ]
