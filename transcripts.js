bb86497d41a662cd0c1c3bb1f734f5f2263b4db5
Jess Marcotte <meaikoh@gmail.com>
2018-01-27 22:50:57 -0500

    Transcripts Added
    Transcripts for Singular They History Lesson, Pronoun Pro 40, and the
    ending added.

--- transcripts.js
+++ transcripts.js
@@ -40,10 +40,58 @@ var story =
 			{ time: 51.728, text: "Trans-Exclusionary Radical Feminists are not feminists. Intersectional feminism is the only feminism." },
 			{ time: 57.765, text: "For more information, listen to marginalized people when they tell you about their lived experiences." }
 		]
-	}
+	},
+		// #3: A Brief History of Singular They
+	{
+		station: 107.0,
+		audioFile: "sounds/singular-they.ogg",
+		transcript:
+		[
+			{ time: 0, text: "You’re listening to the Trans Radio Artists Network in Space." },
+			{ time: 3.545, text: "Here is a brief history of singular they pronouns:" },
+			{ time: 6.717, text: "Chaucer, a famous old dead white dude who wrote about farts and bodily humour a lot, and Shakespeare, an equally if not more famous dead white dude, who is credited with inventing words whenever he wanted, used singular they." },
+			{ time: 17.500, text: "If you add their lifespans together, they lived and collectively used singular they for 108 years. But, other than that, the English language hasn't changed in any way whatsoever since Elizabethan times." },
+			{ time: 26.904, text: "Now, English has no academy that determines correct usage, spelling or grammar rules, unlike some other languages (Here’s looking at you, France!), but nevertheless, in the mid-eighties, most style guides changed their recommendations about singular they to allow its use." },
+			{ time: 39.460, text: "But, since your dad's elementary school teacher said that he should use 'he or she' instead of 'they' when he was a wee cabbage, and since your dad is considered an authority on the English language (right?), the entire world decided that singular they pronouns were shit and to nix them entirely." },
+			{ time: 51.943, text: "So now we're stuck with the gender binary forever, because languages never evolve or change to suit the needs of their speakers. Damn." }
+		]
+	},
+		// #4: Pronoun Pro-40 Advert
+	{
+		station: 108.0,
+		audioFile: "sounds/pronoun-pro.ogg",
+		transcript:
+		[
+			{ time: 0, text: "Do you have a co-worker, friend, frenemy or relative who says they are trying to get your pronouns right but doesn't practice them when you're not there?" },
+			{ time: 8.289, text: "Are you tired of having to do the emotional labour of reassuring them that they're not the worst person in the world just because you correct them about your pronouns?" },
+			{ time: 16.643, text: "Have you hired a friend to follow you around and say your pronouns loudly when people misgender you?" },
+			{ time: 21.995, text: "What if you could spray those pronoun problems away?" },
+			{ time: 25.366, text: "Pronoun Pro-40 is an easy-to-use, no-mess solution for this ever-present problem that plagues so many of us." },
+			{ time: 31.723, text: "Just apply the spray gently to your pronoun problem's face while firmly but softly saying the word 'no.' When they use the correct pronouns, give them a cookie!" },
+			{ time: 40.324, text: "Based on years of testing, we guarantee results, or at the very least, a barrel of laughs. Best of all, you can get this product in an easy-to-assemble kit for our lowest price ever, nine ninety-five! Just add water!" },
+			{ time: 53.138, text: "CALL 1-995-234-5000. That number again is 1-995-234-5000!" }
+		]
+	},
+		// #5: Ending -- change station whenever reflecting a new content entry
+	{
+		station: 99.5,
+		audioFile: "sounds/the-ending.ogg",
+		transcript:
+		[
+			{ time: 0, text: "Welcome to the Headquarters of the Trans Radio Artists Network in Space. We've been expecting you. You persisted, and you listened." },
+			{ time: 8.193, text: "Hopefully some of our broadcasts spoke to you. Maybe you have some stories of your own to share on air." },
+			{ time: 14.043, text: "We're imperfect, and we're fallible, but we're together for now. We don't know what the future will hold, and, well..." },
+			{ time: 21.012, text: "Most of us are tired. We know disappointment all too well." },
+			{ time: 24.539, text: "But somehow, hope springs eternal. So far, we're still here, and so are you. So, welcome. Thanks for listening." }
+		]
+	},
+
 ];
 
 var filler = 
 [
+	{ station: 88.3, audio: new Howl({ src: "sounds/spacechase.ogg", loop: true }) },
+	{ station: 91.7, audio: new Howl({ src: "sounds/beats.ogg", loop: true }) },
+	{ station: 94.5, audio: new Howl({ src: "sounds/bass.ogg", loop: true }) },
 	{ station: 102.4, audio: new Howl({ src: "sounds/country.ogg", loop: true }) }
 ];
